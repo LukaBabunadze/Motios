6052aa7f2feba91964f8c2e5be0febc8ee8e55a6
Luka Babunadze <>
2022-06-22 16:31:35 +0400

    Adding_Main_Topics

--- screens/Menu.tsx
+++ screens/Menu.tsx
@@ -1,6 +1,13 @@
-import { Text, View } from "react-native"
-import { Search, TopicItem } from "../components/atoms";
-import { Topic } from "../components/atoms/Topic";
+import { Text, View, FlatList, StyleSheet } from "react-native"
+import { Search, TopicItem, TestTopic, Title, Topic } from "../components/atoms";
+
+const FakeData = [
+    {title: 'ფსიქოლოგია', imgUrl: 'https://www.all-about-psychology.com/psychology-fb.jpg', key: 1},
+    {title: 'მოტივაცია', imgUrl: 'https://www.all-about-psychology.com/psychology-fb.jpg', key: 2},
+    {title: 'ფილოსოფია', imgUrl: 'https://www.all-about-psychology.com/psychology-fb.jpg', key: 3},
+    {title: 'სავარჯიშო', imgUrl: 'https://www.all-about-psychology.com/psychology-fb.jpg', key: 4},
+    {title: 'სხეულის ენა', imgUrl: 'https://www.all-about-psychology.com/psychology-fb.jpg', key: 5},
+];
 
 export const Menu = () => {
     return(
@@ -8,8 +15,19 @@ export const Menu = () => {
             <Search/> 
             {/* this code should be updated */}
             <TopicItem title="სხეულის ენა" imgUrl="blaa"/>
-            <Topic/> 
+            <TestTopic/> 
+            <Title title="რეკომენდებული" marginHorizontal={20} marginVertical={15}/>
+            <View style={s.topicWrapper}>
+                <FlatList data={FakeData} horizontal showsHorizontalScrollIndicator={false} renderItem={({item}) => (<Topic title={item.title} imgUrl={item.imgUrl}/>)}/>
+            </View>
         </View>
     );
 };
+
+const s = StyleSheet.create({
+    topicWrapper: {
+        marginHorizontal: 15,
+    }
+})
+
 
